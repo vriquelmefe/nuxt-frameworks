8bf2e859a611cc629c481871e5a8dc1cf859cf59
“Vanessa <vriquelmefe@gmail.com>
2021-09-04 00:09:49 -0400

    first commit

---
+++ components/Home.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-container>
+    <v-row >
+      <v-col v-for="personaje in personajes"
+          :key="personaje.id" cols="12" md="6" lg="4">
+        <v-card class="mx-auto" max-width="344">
+          <v-img
+            :src="personaje.imagen"
+            height="200px"
+          ></v-img>
+
+          <v-card-title> {{personaje.nombre}}</v-card-title>
+        </v-card>
+      </v-col>
+      
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  name: "Home",
+
+  data: () => ({
+    personajes: [
+      {
+        id: 1,
+        nombre: "Morty",
+        imagen: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+      },
+      {
+        id: 2,
+        nombre: "Rick",
+        imagen: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+      },
+      {
+        id: 3,
+        nombre: "Summer",
+        imagen: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+      },
+      {
+        id: 4,
+        nombre: "Beth",
+        imagen: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+      },
+      {
+        id: 5,
+        nombre: "Jerry",
+        imagen: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+      },
+    ],
+  }),
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-app app>
+    <v-card
+      color="grey lighten-4"
+      dense
+    >
+      <v-toolbar  >
+        <v-app-bar-nav-icon  @click.stop="drawer = !drawer" ></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Mi App</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn text>Inicio</v-btn>
+          <v-btn text>Buscar</v-btn>
+          <v-btn text>Acerca de</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+      <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      bottom
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="white--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-title>Inicio</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Buscar</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Acerca de</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+  </v-card>
+    <v-main>
+      <v-container>
+        <Home />
+      </v-container>
+    </v-main>
+
+  </v-app>
+</template>
+<script>
+export default {
+  
+  name: "App",
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+
+  watch: {
+    group () {
+      this.drawer = false
+    },
+  },
+};
+</script>
